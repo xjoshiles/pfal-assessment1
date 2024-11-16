a7c4ae8afdc574727b61ef66d79e1506fec97ce8
Josh Iles <JI91@canterbury.ac.uk>
2024-11-16 17:33:14 +0000

    Created models and migrations for flashcards, sets, comments, and collections. Also updated user model and migration. Began work on flashcard_set validator

--- backend/app/validators/register.ts
+++ backend/app/validators/register.ts
@@ -4,20 +4,15 @@ import vine, { SimpleMessagesProvider } from '@vinejs/vine'
 
 const RegisterValidator = vine.compile(
     vine.object({
-        username: vine.string().trim().minLength(3).maxLength(12).unique(async (db, value) => {
-            const user = await db
-                .from('users')
-                .where('username', value)
-                .first()
-                return !user
-            }),
+        username: vine.string().trim().minLength(3).maxLength(12).unique(
+            async (db, value) => !(await db.from('users').where('username', value).first())),
         password: vine.string().trim().minLength(6),
     })
 )
 
-RegisterValidator.messagesProvider = new SimpleMessagesProvider({
-    'unique': 'This {{ field }} is already taken'
-})
+// RegisterValidator.messagesProvider = new SimpleMessagesProvider({
+//     'username.unique': 'This {{ field }} is already taken'
+// })
 
 
 export { RegisterValidator }
